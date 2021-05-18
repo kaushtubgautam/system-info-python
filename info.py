56f971622849d71f662d411c036d9a9f08dbec15
Kaushtub Gautam <67584663+kaushtubgautam@users.noreply.github.com>
2021-05-18 19:33:59 +0530

    Add files via upload

---
+++ info.py
@@ -0,0 +1,11 @@
+#platform-info
+#by @kaushtubgautam
+
+import platform  #pip install platform
+print ("System :" ,platform.system())
+print("Node :",platform.node())
+print("platform :", platform.platform())
+print("Processor ;",platform.processor())
+print('Machine type:', platform.machine())
+
+
